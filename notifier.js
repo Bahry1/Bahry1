9a5fb1c9ff9f576fb7ff3764ff15404501a1fa37
Bahry1 <110595204+Bahry1@users.noreply.github.com>
2025-07-03 21:50:06 +0330

    Update notifier.js

--- notifier.js
+++ notifier.js
@@ -1,15 +1,12 @@
-// notifier.js
 function notify(message, type = "info", duration = 3000) {
   const el = document.getElementById("notifier");
   if (!el) return;
 
   el.textContent = message;
 
-  // حذف کلاس‌های قبلی
   el.className = "";
   el.classList.add("show", type);
 
-  // بستن قبلی (در صورت نیاز)
   clearTimeout(window._notifierTimer);
   window._notifierTimer = setTimeout(() => {
     el.classList.remove("show");
